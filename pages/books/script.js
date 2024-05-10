2981f0c2c5b4862324ce065b4c927d61b0c5ea78
Luan Rafael <luanraffaell@hotmail.com>
2024-05-10 16:07:46 -0300

    feat: pagina de listagem de livros e db com json

---
+++ pages/books/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('../../db/db.json')
+        .then(response => response.json())
+        .then(data => montarView(data.livros))
+        .catch(error => console.log(error))
+});
+function montarView(data) {
+    const container = document.getElementById('container');
+    console.log(data)
+    data.forEach(data => {
+        const a = document.createElement('a');
+        a.href = data.link;
+        a.setAttribute('target', '_blank');
+        a.classList.add('card-book');
+        a.innerHTML = `
+        <div class="card-head">
+        <img src="${data.imgCapa}">
+        </div>
+        <span class="title">${data.titulo}</span>
+        <span class="author">${data.autores.autor1}</span>
+        <span class="price">R$${data.preco}</span>`
+        container.appendChild(a)
+    })
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,27 @@
-
-document.querySelectorAll('.menu-indice').forEach(link =>{
-    link.onclick = function(e){
+document.querySelectorAll('.menu-indice').forEach(link => {
+    link.onclick = function (e) {
         e.preventDefault();
 
         const conteudo = document.getElementById('container')
         fetch(link.href)
             .then(response => response.text())
-            .then(html => conteudo.innerHTML = html)
+            .then(html => {
+                conteudo.innerHTML = html;
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth' // Rola suavemente
+                });
+                // Cria um novo elemento script para cada script na página carregada
+                const scripts = conteudo.querySelectorAll('script');
+                scripts.forEach(script => {
+                    const novoScript = document.createElement('script');
+                    novoScript.innerHTML = script.innerHTML;
+                    conteudo.appendChild(novoScript);
+                });
+                const botao = conteudo.querySelector('#carregar-livros');
+                if(botao){
+                    botao.click()
+                }
+            })
     }
 })
